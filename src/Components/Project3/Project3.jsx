165c98b912af81690df98159dc2687fa15102557
John Rogee Turqueza <jrogeet@gmail.com>
2025-02-09 16:48:09 +0800

    - StepMessage as a separate component and applying children prop - Component inside an HTML element INSIDE a Component

--- src/Components/Project3/Project3.jsx
+++ src/Components/Project3/Project3.jsx
@@ -44,9 +44,23 @@ function Steps() {
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          {/* <p className="message">
+            <h3>Step {step}:</h3> {messages[step - 1]}
+          </p> */}
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button bgColor="#e7e7e7" textColor="#333" 
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}>
+                  Learn how
+              </Button>
+            </div>
+          </StepMessage>
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+          </StepMessage>
 
           <div className="buttons">
             <Button 
@@ -66,6 +80,15 @@ function Steps() {
   );
 }
 
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+}
+
 function Button({ textColor, bgColor, onClick, children }) {
   return (
     <button
@@ -81,6 +104,10 @@ export default function Project3() {
   return (
     <div>
       <Steps />
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>*</p>
+      </StepMessage>
     </div>
   );
 }
