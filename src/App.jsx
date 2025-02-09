8386afd4ddc087bd1937f25a917d3ab657955fea
John Rogee Turqueza <jrogeet@gmail.com>
2025-02-09 17:08:06 +0800

    - Initialized project/exercise 7: accordion v2 - Makes the accordion only open 1 item at a time

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Project3 from "./Components/Project3/Project3";
 import Project4 from "./Components/Project4/Project4";
 import Project5 from "./Components/Project5/Project5";
 import Project6 from "./Components/Project6/Project6";
+import Project7 from "./Components/Project7/Project7";
 import "./App.css";
 
 export default function App() {
@@ -32,6 +33,10 @@ export default function App() {
       <details>
         <summary>Project 6: Accordion v1</summary>
         <Project6 />
+      </details>
+      <details>
+        <summary>Project 7: Accordion v2</summary>
+        <Project7 />
       </details>
     </div>
   );
---
+++ src/Components/Project7/Project7.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import "./Project7.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  }
+];
+
+function Accordion({ data }) {
+  const [curOpen, setIsOpen] = useState(null);
+
+  return(
+        <div className="accordion">
+            {data.map((el, index) => 
+                <AccordionItem 
+                    curOpen={curOpen}
+                    onOpen={setIsOpen}
+                    title={el.title} 
+                    num={index} 
+                    key={el.title}>
+                  {el.text} 
+                </AccordionItem>
+            )}
+
+            <AccordionItem 
+                curOpen={curOpen}
+                onOpen={setIsOpen}
+                title='Test 1' 
+                num={22} 
+                key='test 1'>
+              <p>Excepturi velit laborum, perspiciatis nemo</p>
+              <ul>
+                <li>osadjwoqhdiopwqhd0owqhjipodhwqpoi</li>
+                <li>osadjwoqhdiopwqhd0owqhjipodhwqpoi</li>
+                <li>osadjwoqhdiopwqhd0owqhjipodhwqpoi</li>
+              </ul>
+            </AccordionItem>
+        </div>
+    );
+}
+
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
+
+
+  function handleToggle() {
+      // setIsOpen((isOpen) => !isOpen);
+      onOpen(isOpen ? null : num);
+  }
+
+  return (
+      <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+          <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+          <p className="title">{title}</p>
+          <p className="icon">{isOpen ? "-" : "+"}</p>
+          
+          {isOpen && <div className="content-box">{children}</div>}
+      </div>
+  );
+}
+
+export default function Project7() {
+  return (
+    <div>
+      <Accordion data={faqs}/>
+    </div>
+  );
+}
